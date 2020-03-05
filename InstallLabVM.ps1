214981aa0f4b82802b4ddddc8bf93b8ba6f14cc0
Dan Patrick <dan@clouddeltav.com>
2020-03-05 11:05:42 -0500

    update to remove vs2017

--- InstallLabVM.ps1
+++ InstallLabVM.ps1
@@ -9,31 +9,11 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 $Packages = 'googlechrome',`
             'docker-for-windows',`
             'visualstudiocode',`
-            'git',`
-            'visualstudio2017community',`
-            'visualstudio2017-workload-azure',`
-            'visualstudio2017-workload-manageddesktop',`
-            'visualstudio2017-workload-netweb',`
-            'dotnetcore-sdk'
+            'git'
 
 #Install Packages
 ForEach ($PackageName in $Packages)
 {choco install $PackageName -y}
-
-#Update Visual Studio
-$command1 = @'
-cmd.exe /C C:\Users\stormtrooperio\AppData\Local\Temp\chocolatey\visualstudio2017community\15.2.26430.20170605\vs_community.exe --update --quiet --wait
-'@
-$command2 = @'
-cmd.exe /C C:\Users\stormtrooperio\AppData\Local\Temp\chocolatey\visualstudio2017community\15.2.26430.20170605\vs_community.exe --update --quiet --wait --passive --norestart --installPath `
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community"
-'@
-Set-ExecutionPolicy Bypass -Scope Process -Force
-Invoke-Expression -Command:$command1
-Invoke-Expression -Command:$command2
-
-#Install Visual Studio Code Extensions
-& 'C:\Program Files\Microsoft VS Code\bin\code.cmd' --install-extension ms-vscode.csharp
 
 #Add Demo User to docker group
 Add-LocalGroupMember -Member stormtrooperio -Group docker-users
